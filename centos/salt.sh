20864ee70542b8a862fc93afb7c592eedb72a8b1
Mingchin Hsieh <mingchinhsieh@Mingchins-MacBook-Pro.local>
2016-07-04 01:09:33 +0800

    fix test.ping and add install_package

--- centos/salt.sh
+++ centos/salt.sh
@@ -17,25 +17,13 @@ st2 run packs.download packs=salt
 st2 run packs.setup_virtualenv packs=salt
 
 # Overwrite config.yaml
-cp -f /vagrant/chatops/opt/stackstorm/packs/salt/config.yaml /opt/stackstorm/packs/salt/config.yaml
+rsync -avz /vagrant/chatops/opt/stackstorm/packs/salt/ /opt/stackstorm/packs/salt/
 
 # Register all actions contained in the pack:
 st2 run packs.load register=all
 
-# Install our custom pack with ansible ChatOps command aliases
-#st2 run packs.install packs=st2-chatops-aliases repo_url=armab/st2-chatops-aliases
-
-# Needed by salt-ssh
-yum -y install sshpass
-
 # cowsay via ChatOps
 rpm -U http://www.melvilletheatre.com/articles/el7/cowsay-3.03-14.el7.centos.noarch.rpm
-
-# Copy ansible config files from vagrant shared directory to '/etc/ansible'
-#st2 run ansible.command_local module_name=synchronize args='src=/vagrant/ansible/ dest=/etc/ansible'
-#chown -R root:root /etc/ansible
-#chmod -R 755 /etc/ansible
-#chmod 640 $(find /etc/ansible -type f)
 
 st2 action list --pack=salt
 
--- ubuntu/salt.sh
+++ ubuntu/salt.sh
@@ -18,25 +18,13 @@ st2 run packs.download packs=salt
 st2 run packs.setup_virtualenv packs=salt
 
 # Overwrite config.yaml
-cp -f /vagrant/chatops/opt/stackstorm/packs/salt/config.yaml /opt/stackstorm/packs/salt/config.yaml
+rsync -avz /vagrant/chatops/opt/stackstorm/packs/salt/ /opt/stackstorm/packs/salt/
 
 # Register all actions contained in the pack:
 st2 run packs.load register=all
 
-# Install our custom pack with ansible ChatOps command aliases
-#st2 run packs.install packs=st2-chatops-aliases repo_url=armab/st2-chatops-aliases
-
-# Needed by salt-ssh
-apt-get install -y sshpass
-
 # cowsay via ChatOps
 apt-get install -y cowsay
-
-# Copy ansible config files from vagrant shared directory to '/etc/ansible'
-#st2 run ansible.command_local module_name=synchronize args='src=/vagrant/ansible/ dest=/etc/ansible'
-#chown -R root:root /etc/ansible
-#chmod -R 755 /etc/ansible
-#chmod 640 $(find /etc/ansible -type f)
 
 st2 action list --pack=salt
 
@@ -44,4 +32,3 @@ st2 run salt.runner_manage.up
 
 echo "Done!"
 exit 0
-
